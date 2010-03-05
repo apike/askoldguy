e4b966fce5a1cd7095a8a0408379843220e409c8
Bruce Alderson <mx@dogbert-2.local>
2010-03-04 21:12:01 -0800

    Added ask page template.

---
+++ wp-content/themes/oldguy/ask.php
@@ -0,0 +1,62 @@
+<?php
+/*
+Template Name: Ask
+*/
+?>
+
+<?php get_header(); ?>
+
+<div id="content">
+
+<?php 
+if (have_posts()) {
+	while (have_posts()) {
+		the_post(); 
+?>
+		<div class="post">
+		<h2><?php the_title(); ?></h2>
+		<?php edit_post_link('edit'); ?>
+		<?php the_content(); ?> 
+		</div>
+<?php 
+	} 
+} ?>
+
+<div id="question">
+
+<form action="<?php echo get_option('siteurl'); ?>/wp-comments-post.php" method="post">
+
+	<ul>
+		<li>
+			<label for="author">Name</label>	
+			<input type="text" name="author" id="author" value="<?php echo $comment_author; ?>" placeholder="First and Last name" autofocus />
+		</li>
+
+		<li>
+			<label for="email">Email</label>
+			<input type="text" name="email" id="email" value="<?php echo $comment_author_email; ?>" placeholder="you@yourdomain.com (optional)" />
+
+		</li>
+
+		<li>
+			<label for="url">Website</label>
+			<input type="text" name="url" id="url" value="<?php echo $comment_author_url; ?>" placeholder="http://yourdomain.com (optional)" />
+		</li>
+
+		<li>
+			<textarea name="comment"></textarea>
+		</li>
+		<li>
+			<input name="submit" type="submit" id="submit" value="Send it!" />
+			<input type="hidden" name="comment_post_ID" value="<?php echo $id; ?>" />
+			<input type="hidden" name="redirect_to" value="<?php echo get_option('siteurl'); ?>/contact/thanks" />
+		</li>
+<?php do_action('comment_form', $post->ID); ?>
+</ul>
+</form>
+</div>
+
+
+</div>
+
+<?php get_footer(); ?>
